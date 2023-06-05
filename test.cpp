b612d3ae0e3878e9e90c00413619bdaba7837ae1
KamilHanna <131144516+KamilHanna@users.noreply.github.com>
2023-06-05 17:04:04 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
-#this is a test
-#test number 24
-#test number modified
-#22666
-#hellooo66
-#88888888hhh8
-#u888
+/*this is a test
+test number 24
+test number modified
+22666
+hellooo66
+88888888hhh8
+u888
+*/
